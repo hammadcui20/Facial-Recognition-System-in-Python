8d24a20aec2533b35e28215c71d58e25a828c8b2
Hammad Hussain <hammadasmat@gmail.com>
2023-06-13 09:03:09 +0500

    init commit

---
+++ dataset.py
@@ -0,0 +1,34 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri May 20Q 00:18:07 2023
+
+@author: dell
+"""
+import cv2
+import os
+
+cam = cv2.VideoCapture(0)
+cam.set(3, 640)
+cam.set(4, 480)
+face_detector = cv2.CascadeClassifier('D:\Project AI\Cascades\haarcascade_frontalface_default.xml')
+face_id = input('\n enter user id end press <return> ==>  ')
+print("\n [INFO] Initializing face capture. Look the camera and wait ...")
+count = 0
+while(True):
+    ret, img = cam.read()
+    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    faces = face_detector.detectMultiScale(gray, 1.3, 5)
+    for (x,y,w,h) in faces:
+        cv2.rectangle(img, (x,y), (x+w,y+h), (255,0,0), 2)     
+        count += 1
+        cv2.imwrite("D:\Project AI\FacialRecognitionProject\dataset/User."+ str(face_id) + '.' + str(count) + ".jpg", gray[y:y+h,x:x+w])
+        cv2.imshow('image', img)
+    k = cv2.waitKey(100) & 0xff
+    if k == 27:
+        break
+    elif count >= 30:
+         break
+print("\n [INFO] Exiting Program and cleanup stuff")
+cam.release()
+cv2.destroyAllWindows()
+
